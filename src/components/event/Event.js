81da66f52c76221257749fb687cdd8529fd6a9e2
Clay Mitchell <clay@pfd.net>
2020-03-02 20:22:51 -0500

    got rid of so much wasted vertical space

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -1,52 +1,79 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 
-import API from '@aws-amplify/api'
+import API from "@aws-amplify/api";
 
-import { listEventInstancesByDate } from './eventQueries'
-import { EventInstanceContext } from './EventInstanceContext';
-import EventInstance from './EventInstance';
-import { AuthContext } from './../user/AuthContext';
+import { listEventInstancesByDate } from "./eventQueries";
+import { EventInstanceContext } from "./EventInstanceContext";
+import EventInstance from "./EventInstance";
+import { AuthContext } from "./../user/AuthContext";
+import { Grid, Typography } from "@material-ui/core";
+import { format, parseISO } from 'date-fns'
+import logo from "../../images/omh.svg";
 
 const Event = () => {
     const user = useContext(AuthContext);
     const [eventInstance, setEventInstance] = useState(null);
 
     useEffect(() => {
-        let authmode = "AWS_IAM"
-        if(user) {
-            authmode = "AMAZON_COGNITO_USER_POOLS"
+        let authmode = "AWS_IAM";
+        if (user) {
+            authmode = "AMAZON_COGNITO_USER_POOLS";
         }
-        API.graphql(
-            {
-                query: listEventInstancesByDate, 
-                variables: {"date": new Date().toISOString().substring(0,10)},
-                authMode: authmode
-            }).then(eventInstanceData => {
-                console.log("Event Loadded")
+        API.graphql({
+            query: listEventInstancesByDate,
+            variables: { date: new Date().toISOString().substring(0, 10) },
+            authMode: authmode
+        })
+            .then((eventInstanceData) => {
+                console.log("Event Loadded");
                 console.log("User ", user);
-                console.log('authmode ', authmode);
-                setEventInstance(eventInstanceData.data.listEventInstances.items[0]);
-            }).catch(err => {
-                console.log("Error loading Event")
-                console.log("User ", user);
-                console.log('authmode ', authmode);
-                console.log("Error: ", err);
+                console.log("authmode ", authmode);
+                setEventInstance(
+                    eventInstanceData.data.listEventInstances.items[0]
+                );
             })
-    }, [user])
+            .catch((err) => {
+                console.log("Error loading Event");
+                console.log("User ", user);
+                console.log("authmode ", authmode);
+                console.log("Error: ", err);
+            });
+    }, [user]);
 
-    if(!eventInstance) {
-        return (
-            <div>Loading...</div>
-        )
+    if (!eventInstance) {
+        return <div>Loading...</div>;
     }
-
+    let dt = parseISO(`${eventInstance.date}T${eventInstance.event.defaultTime}`);
     return (
-        <EventInstanceContext.Provider value={{eventInstance, setEventInstance}}>
-            <div>
-                <EventInstance />
-            </div>
-        </EventInstanceContext.Provider>
-    )
-}
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={3}>
+                    <img
+                        src={logo}
+                        alt="Old Man Hoops Logo"
+                        style={{ marginTop: 3 }}
+                    />
+                </Grid>
+                <Grid item xs={9}>
+                    <Typography variant="h3" >
+                        {eventInstance.event.name}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                    {format(dt, 'EEEE, MMMM do yyyy')}
+                    <br/>
+                    {format(dt, 'hh:mm a')}
+                    </Typography>
+                </Grid>
+            </Grid>
+            <EventInstanceContext.Provider
+                value={{ eventInstance, setEventInstance }}
+            >
+                <div>
+                    <EventInstance />
+                </div>
+            </EventInstanceContext.Provider>
+        </>
+    );
+};
 
 export default Event;
